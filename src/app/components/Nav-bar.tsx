29242a63bb6542059dbb76a4448661670c09da8a
Kian Bernardo <akian.bernardo11@gmail.com>
2026-05-01 05:01:48 +0800

    Initial commit for watering web project

---
+++ src/app/components/Nav-bar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-center w-full h-fit  text-black border-2 bg-[#00fa9a] p-5">
+      <ul className="flex justify-center space-x-5  ">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+}
